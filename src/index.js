01c875bb9799eb22a1256153626a3567447524a4
Marushevskyi Ivan <131617971+MarushevskyiIvan@users.noreply.github.com>
2023-09-24 14:10:54 +0300

    10.2

--- src/index.js
+++ src/index.js
@@ -5,51 +5,72 @@
 const API_KEY =
   'live_zr60gtmYway2jnmViOgPzPnqk2naOknryCdDGPr2wxWgNvyf71N5SCBtIUltTvla';
 const selectEl = document.querySelector('.breed-select');
+const divEl = document.querySelector('.cat-info');
+
+selectEl.addEventListener('change', fetchCatByBreed);
 
 fetchBreeds();
 
 function fetchBreeds() {
-  fetch('https://api.thecatapi.com/v1/breeds?&breed_ids=beng&key=${API_KEY}')
+  fetch(
+    `https://api.thecatapi.com/v1/breeds?&breed_ids=beng&?api_key=${API_KEY}`
+  )
     .then(response => {
       if (!response.ok) {
         throw new Error(response.status);
       }
-      console.log(response);
       return response.json();
     })
     .then(data => {
-      // console.log(data);
-      const element = data
-        .map(({ name, id }) => {
-          const optionMarkup = `<option value="${id}">${name}</option>`;
-          selectEl.insertAdjacentHTML('beforeend', optionMarkup);
-          const breedId = id;
-        })
-        .join('');
+      console.log(data);
+      return selectMarkup(data);
     })
     .catch(error => {
       console.warn('Network Error', error);
     });
 }
 
-function fetchCatByBreed(breedId) {
-  fetch('https://api.thecatapi.com/v1/images/search?breed_ids=${item.id}')
+function selectMarkup(data) {
+  return data
+    .map(({ name, id }) => {
+      const optionMarkup = `<option value="${id}">${name}</option>`;
+      selectEl.insertAdjacentHTML('beforeend', optionMarkup);
+      breedId = id;
+      fetchCatByBreed(id);
+    })
+    .join('');
+}
+
+function fetchCatByBreed(id) {
+  fetch(
+    `https://api.thecatapi.com/v1/images/search?breed_ids=${id}&?api_key=${API_KEY}`
+  )
     .then(response => {
       if (!response.ok) {
         throw new Error(response.status);
       }
-
+      // console.log(response);
       return response.json();
     })
     .then(data => {
       console.log(data);
-      // const element = data.map(item => {
-      //   console.log(item.name);
-      //   const optionMarkup = `<option value="${item.id}">${item.name}</option>`;
-      //   selectEl.insertAdjacentHTML('beforeend', optionMarkup);
-      // });
+
+      return catInfoMarkup(data);
     })
     .catch(error => {
       console.warn('Network Error', error);
     });
+}
+
+function catInfoMarkup(data) {
+  return data
+    .map(({ name, description, url, temperament }) => {
+      const infoMarkup = `
+  <img src="${url}" width="300">
+  <h3>${name}</h3>
+  <p>${description}</p>
+  <p>${temperament}</p>`;
+      divEl.insertAdjacentHTML('beforeend', infoMarkup);
+    })
+    .join('');
 }
