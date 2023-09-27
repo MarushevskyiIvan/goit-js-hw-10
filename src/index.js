39a175a1e192c4369e621264c8b6b69b1781ba5e
Marushevskyi Ivan <131617971+MarushevskyiIvan@users.noreply.github.com>
2023-09-27 19:33:55 +0300

    10.5

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import { refs } from './js/refs';
 import { fetchBreeds } from './js/cat-api';
-import { onSelectElChange } from './js/cat-api';
+import { catInfoMarkup } from './js/markup';
 import { selectMarkup } from './js/markup';
+import { fetchCatByBreed } from './js/cat-api';
 
 refs.selectEl.addEventListener('change', onSelectElChange);
+
 refs.selectEl.classList.add('visually-hidden');
 refs.loaderEl.classList.remove('visually-hidden');
 
@@ -20,3 +22,17 @@ fetchBreeds()
     refs.errorEl.classList.remove('visually-hidden');
     console.warn('Network Error', error);
   });
+
+function onSelectElChange(evt) {
+  selectValue = evt.target.value;
+  refs.divEl.innerHTML = '';
+  refs.loaderEl.classList.remove('visually-hidden');
+  fetchCatByBreed(selectValue)
+    .then(result => {
+      refs.loaderEl.classList.add('visually-hidden');
+      catInfoMarkup(result);
+    })
+    .catch(error => {
+      console.warn('Network Error', error);
+    });
+}
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,6 +1,3 @@
-import { refs } from './refs';
-import { catInfoMarkup } from './markup';
-
 const API_KEY =
   'live_zr60gtmYway2jnmViOgPzPnqk2naOknryCdDGPr2wxWgNvyf71N5SCBtIUltTvla';
 const BASE_URL = 'https://api.thecatapi.com/v1';
@@ -8,7 +5,6 @@ const BASE_URL = 'https://api.thecatapi.com/v1';
 export function fetchBreeds() {
   return fetch(`${BASE_URL}/breeds?&breed_ids=beng&?api_key=$ {API_KEY}`).then(
     response => {
-      // refs.loaderEl.classList.remove('visually-hidden');
       if (!response.ok) {
         throw new Error(response.status);
       }
@@ -26,19 +22,4 @@ export function fetchCatByBreed(id) {
     }
     return response.json();
   });
-}
-
-export function onSelectElChange(evt) {
-  const selectValue = evt.target.value;
-  refs.divEl.innerHTML = '';
-  refs.loaderEl.classList.remove('visually-hidden');
-
-  fetchCatByBreed(selectValue)
-    .then(result => {
-      refs.loaderEl.classList.add('visually-hidden');
-      catInfoMarkup(result);
-    })
-    .catch(error => {
-      console.warn('Network Error', error);
-    });
 }
